4c5eb5613bd74c8a86f2f52f2559bdb9a0059dfc
TakutoJibiki <jibikitakuto@gmail.com>
2023-10-17 16:23:15 +0900

    csv から gif を作る関数を追加

--- gif.py
+++ gif.py
@@ -1,5 +1,8 @@
 from PIL import Image
 from glob import glob
+import pandas as pd
+import matplotlib.pyplot as plt
+import os, shutil
 
 def make_gif(dir, gif_name, sampling=1, duration=100):
     assert type(sampling) == int
@@ -19,3 +22,39 @@ def make_gif(dir, gif_name, sampling=1, duration=100):
         duration=duration,
         loop = 0,
     )
+
+
+def make_gif_from_csv(csv_path, gif_name, header=None, labels=None, marker=None, sampling=1, duration=100):
+    """make_gif_from_csv
+    
+    指定した CSV からプロットのアニメーション GIF を生成する
+    
+    """
+    table = pd.read_csv(csv_path, header=header).values
+
+    assert not os.path.isdir('tmp')
+    os.makedirs('tmp')
+
+    min_x = min(table[:, 0])
+    max_x = max(table[:, 0])
+    min_y = min(table[:, 1])
+    max_y = max(table[:, 1])
+    plt.rcParams['font.size'] = 12
+    plt.rcParams['font.family'] = 'Times New Roman'
+    plt.rcParams['mathtext.fontset'] = 'cm'
+    for i in range(table.shape[0]):
+        plt.cla()
+        plt.clf()
+        plt.xlim(min_x*1.1, max_x*1.1)
+        plt.ylim(min_y*1.1, max_y*1.1)
+        plt.xlabel(labels[0])
+        plt.ylabel(labels[1])
+        plt.plot(table[:i+1, 0], table[:i+1, 1], marker=marker)
+        plt.savefig(f'tmp/{i}.png')
+
+    make_gif('tmp', gif_name, sampling, duration)
+    shutil.rmtree('tmp')
+
+
+if __name__ == '__main__':
+    make_gif_from_csv(csv_path='4500.csv', gif_name='anim.gif', header=0, labels=['$H$', '$B$'], duration=300, marker='')
